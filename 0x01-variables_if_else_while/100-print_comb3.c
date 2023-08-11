3134b4a0c68d0df65c39f78d140e4b1979936290
AKinee Essential <akineeessential@gmail.com>
2023-08-11 16:02:25 -0700

    combining two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,19 @@ int main(void)
 	int ones;
 
 	for (tens = 0; tens <= 9; tens++)
-		{
-			for (ones = tens + 1; ones <= 9; ones++)
-				{	
-					putchar(tens + '0');
-					putchar(ones + '0');
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{	
+			putchar(tens + '0');
+			putchar(ones + '0');
 
-					if (tens < 8)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+			if (tens < 8)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 		}
+	}
 	putchar('\n');
 
 	return (0);
