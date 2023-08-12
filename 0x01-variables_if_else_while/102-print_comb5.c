cdc0f7cb981b92d7cccd2c2c855e1ce9b686fe0b
AKinee Essential <akineeessential@gmail.com>
2023-08-12 08:23:30 -0700

    printing double numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Print all possible combinations of single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f <= 98; f++)
+	{
+	for (s = f +1; s <= 99; s++)
+	{
+	putchar((f / 10) + '0');
+	putchar((f % 10) + '0');
+	putchar(' ');
+	putchar((f / 10) + '0');
+	putchar((f % 10) + '0');
+	if (f == 98 && s == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
