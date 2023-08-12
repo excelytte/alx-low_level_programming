1482f2b74c681a62bc5b2bf6f306476921788abe
AKinee Essential <akineeessential@gmail.com>
2023-08-12 08:30:29 -0700

    printing double numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (f = 0; f <= 98; f++)
 	{
-	for (s = f +1; s <= 99; s++)
+	for (s = f + 1; s <= 99; s++)
 	{
 	putchar((f / 10) + '0');
 	putchar((f % 10) + '0');
 	putchar(' ');
-	putchar((f / 10) + '0');
-	putchar((f % 10) + '0');
+	putchar((s / 10) + '0');
+	putchar((s % 10) + '0');
 	if (f == 98 && s == 99)
 	continue;
 	putchar(',');
