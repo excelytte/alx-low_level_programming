2abe544063ac874257b88a57ce4ac2c3667ca9e0
Akinee Essential <akineeessential@gmail.com>
2023-08-15 15:49:00 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char l;
+	int i = 0;
+
+	while (i <= 10)
+	{
+		l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
