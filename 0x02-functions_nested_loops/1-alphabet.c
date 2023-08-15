b90ed747c1f3ab500f44e7b5efb8b36363eee546
Akinee Essential <akineeessential@gmail.com>
2023-08-15 15:21:19 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+    print_alphabet(void)
+    {
+	    for (i = 'a'; i <= 'z'; i++)
+	    {
+	    _putchar(i);
+	    }
+	    _putchar('\n');
+    return (0);
+}
